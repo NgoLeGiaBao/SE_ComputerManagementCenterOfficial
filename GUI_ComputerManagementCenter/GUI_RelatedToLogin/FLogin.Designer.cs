36e03b4ee5619d52e5483c9337b975c4623c3427
Nguyễn Nam Hoàng <112799623+nnhoanghd2004@users.noreply.github.com>
2024-04-08 00:54:52 +0700

    hoang pushhhhh

--- GUI_ComputerManagementCenter/GUI_RelatedToLogin/FLogin.Designer.cs
+++ GUI_ComputerManagementCenter/GUI_RelatedToLogin/FLogin.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.gunaTextBox1 = new Guna.UI.WinForms.GunaTextBox();
+            this.gunaTextBox2 = new Guna.UI.WinForms.GunaTextBox();
             this.SuspendLayout();
             // 
             // gunaTextBox1
@@ -40,19 +41,37 @@
             this.gunaTextBox1.FocusedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
             this.gunaTextBox1.FocusedForeColor = System.Drawing.SystemColors.ControlText;
             this.gunaTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.gunaTextBox1.Location = new System.Drawing.Point(243, 147);
+            this.gunaTextBox1.Location = new System.Drawing.Point(299, 105);
             this.gunaTextBox1.Name = "gunaTextBox1";
             this.gunaTextBox1.PasswordChar = '\0';
             this.gunaTextBox1.SelectedText = "";
             this.gunaTextBox1.Size = new System.Drawing.Size(160, 32);
             this.gunaTextBox1.TabIndex = 0;
-            this.gunaTextBox1.Text = "gunaTextBox1";
+            this.gunaTextBox1.Text = "Hoàng";
+            // 
+            // gunaTextBox2
+            // 
+            this.gunaTextBox2.BaseColor = System.Drawing.Color.White;
+            this.gunaTextBox2.BorderColor = System.Drawing.Color.Silver;
+            this.gunaTextBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.gunaTextBox2.FocusedBaseColor = System.Drawing.Color.White;
+            this.gunaTextBox2.FocusedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.gunaTextBox2.FocusedForeColor = System.Drawing.SystemColors.ControlText;
+            this.gunaTextBox2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaTextBox2.Location = new System.Drawing.Point(299, 212);
+            this.gunaTextBox2.Name = "gunaTextBox2";
+            this.gunaTextBox2.PasswordChar = '\0';
+            this.gunaTextBox2.SelectedText = "";
+            this.gunaTextBox2.Size = new System.Drawing.Size(160, 32);
+            this.gunaTextBox2.TabIndex = 1;
+            this.gunaTextBox2.Text = "Hoàng";
             // 
             // FLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gunaTextBox2);
             this.Controls.Add(this.gunaTextBox1);
             this.Name = "FLogin";
             this.Text = "FLogin";
@@ -63,5 +82,6 @@
         #endregion
 
         private Guna.UI.WinForms.GunaTextBox gunaTextBox1;
+        private Guna.UI.WinForms.GunaTextBox gunaTextBox2;
     }
 }
